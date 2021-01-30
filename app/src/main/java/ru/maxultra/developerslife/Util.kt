9383927781a6782991d92af5df43efad3e562960
Maxim Moschenskikh <m.moschenskikh@gmail.com>
2021-01-30 17:43:08 +0300

    Loading circle drawable added

--- app/src/main/java/ru/maxultra/developerslife/Util.kt
+++ app/src/main/java/ru/maxultra/developerslife/Util.kt
@@ -1,6 +1,9 @@
 package ru.maxultra.developerslife
 
+import android.content.Context
 import android.content.res.Resources
+import android.graphics.drawable.Drawable
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 
 fun getTabText(position: Int, resources: Resources): String {
     return when (position) {
@@ -10,4 +13,13 @@ fun getTabText(position: Int, resources: Resources): String {
         3 -> resources.getString(R.string.top)
         else -> throw IllegalStateException("Unknown tab (number $position)")
     }
+}
+
+fun getLoadingDrawable(context: Context): Drawable {
+    val drawable = CircularProgressDrawable(context)
+    drawable.centerRadius = 40f
+    drawable.strokeWidth = 10f
+    drawable.setColorSchemeColors(context.getColor(R.color.peter_river))
+    drawable.start()
+    return drawable
 }
