26c01d3d9a2fea8ee9155754556d7584d75d17e4
Maxim Moschenskikh <m.moschenskikh@gmail.com>
2021-01-30 18:24:29 +0300

    Basic navigation implemented

--- app/src/main/java/ru/maxultra/developerslife/ui/gifpage/PageViewModel.kt
+++ app/src/main/java/ru/maxultra/developerslife/ui/gifpage/PageViewModel.kt
@@ -1,9 +1,6 @@
 package ru.maxultra.developerslife.ui.gifpage
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import kotlinx.coroutines.launch
 import ru.maxultra.developerslife.network.GifApi
 import ru.maxultra.developerslife.network.NetworkGif
@@ -20,8 +17,15 @@ class PageViewModel : ViewModel() {
     val gif: LiveData<NetworkGif>
         get() = _gif
 
+    private val previousGifs = mutableListOf<NetworkGif>()
+
+    private val position = MutableLiveData<Int>()
+
+    val prevEnabled: LiveData<Boolean> = Transformations.map(position) { pos -> pos != 0 }
+
     init {
         getGifPost()
+        position.value = 0
     }
 
     private fun getGifPost() {
@@ -29,10 +33,25 @@ class PageViewModel : ViewModel() {
             _status.value = GifApiStatus.LOADING
             try {
                 _gif.value = GifApi.retrofitService.getRandomImage()
+                gif.value?.let { previousGifs.add(it) }
                 _status.value = GifApiStatus.DONE
             } catch (e: Exception) {
                 _status.value = GifApiStatus.ERROR
             }
         }
+    }
+
+    fun onNext() {
+        position.value = position.value?.plus(1) ?: previousGifs.size - 1
+        if (position.value == previousGifs.size) {
+            getGifPost()
+        } else {
+            _gif.value = previousGifs[position.value!!]
+        }
+    }
+
+    fun onPrev() {
+        position.value = position.value?.minus(1) ?: 0
+        _gif.value = previousGifs[position.value!!]
     }
 }
