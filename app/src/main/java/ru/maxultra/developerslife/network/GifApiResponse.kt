4e28b2e17c919c3b2130b0ec56898b17cdfcb686
Maxim Moschenskikh <m.moschenskikh@gmail.com>
2021-01-31 02:48:41 +0300

    Updated network API

--- app/src/main/java/ru/maxultra/developerslife/network/GifApiResponse.kt
+++ app/src/main/java/ru/maxultra/developerslife/network/GifApiResponse.kt
@@ -2,8 +2,12 @@ package ru.maxultra.developerslife.network
 
 import com.squareup.moshi.Json
 
-class NetworkGif(
+data class NetworkGif(
     val id: String,
     @Json(name = "gifURL") val url: String,
     @Json(name = "description") val title: String
+)
+
+data class ResponseGifArray(
+    @Json(name = "result") val gifs: List<NetworkGif>
 )
--- app/src/main/java/ru/maxultra/developerslife/network/GifApiService.kt
+++ app/src/main/java/ru/maxultra/developerslife/network/GifApiService.kt
@@ -5,8 +5,10 @@ import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 private const val BASE_URL = "http://developerslife.ru"
+private const val JSON_TRUE = "?json=true"
 
 private val moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
@@ -18,8 +20,14 @@ private val retrofit = Retrofit.Builder()
     .build()
 
 interface GifApiService {
-    @GET("random?json=true")
+    @GET("random$JSON_TRUE")
     suspend fun getRandomImage(): NetworkGif
+
+    @GET("{section}/{page}$JSON_TRUE")
+    suspend fun getSectionImages(
+        @Path("section") section: String,
+        @Path("page") page: Int
+    ): ResponseGifArray
 }
 
 object GifApi {
@@ -27,3 +35,5 @@ object GifApi {
         retrofit.create(GifApiService::class.java)
     }
 }
+
+enum class GifSection(val alias: String) { RANDOM(""), LATEST("latest"), TOP("top") }
