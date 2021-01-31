2b2e03be5e877646426a7f3ee31d105d65c76752
Maxim Moschenskikh <m.moschenskikh@gmail.com>
2021-01-31 03:52:18 +0300

    Incorrect tab name fixed

--- app/src/main/java/ru/maxultra/developerslife/util/Util.kt
+++ app/src/main/java/ru/maxultra/developerslife/util/Util.kt
@@ -10,8 +10,7 @@ fun getTabText(position: Int, resources: Resources): String {
     return when (position) {
         0 -> resources.getString(R.string.random)
         1 -> resources.getString(R.string.latest)
-        2 -> resources.getString(R.string.hot)
-        3 -> resources.getString(R.string.top)
+        2 -> resources.getString(R.string.top)
         else -> throw IllegalStateException("Unknown tab (number $position)")
     }
 }
